6049a94a636cbb7ee9695cade2965832d0f637fc
Martin Dobrev <martindobrev87@gmail.com>
2022-06-09 11:50:51 +0300

    Bg image updated

--- pages/index.js
+++ pages/index.js
@@ -46,64 +46,66 @@ export default function Home() {
         ></link>
       </Head>
 
-      <div className="container py-5 py-lg-6">
-        <LogoLink />
+      <div className="container-homepage-wrapper">
+        <div className="container py-5 py-lg-6">
+          <LogoLink />
 
-        <div className="row align-items-center mt-6 mt-lg-15">
-          <div className="col-lg-6 col-xl-5 mt-6 mt-lg-0">
-            <h1 className="text-hero ">
-              Get Ready
-              <br />
-              for <span className="text-bold"> Heliswap!</span>
-            </h1>
+          <div className="row align-items-center mt-6 mt-lg-15">
+            <div className="col-lg-6 col-xl-5 mt-6 mt-lg-0">
+              <h1 className="text-hero ">
+                Get Ready
+                <br />
+                for <span className="text-bold"> Heliswap!</span>
+              </h1>
 
-            <p className="text-main text-slate mt-5 mt-lg-5">
-              The first DEX for the Hedera ecosystem supporting cross-chain swaps between the Hedera
-              and Ethereum ecosystems.
-            </p>
+              <p className="text-main text-slate mt-5 mt-lg-5">
+                The first DEX for the Hedera ecosystem supporting cross-chain swaps between the
+                Hedera and Ethereum ecosystems.
+              </p>
 
-            {formSent ? (
-              <div className="alert alert-success mt-5 mt-lg-5" role="alert">
-                <strong>Holy guacamole!</strong> Thanks for your interest!.
-              </div>
-            ) : (
-              <>
-                <p className="text-lead text-bold mt-5 mt-lg-5">Get notified when we launch</p>
-
-                <div className="mt-4">
-                  <div className="position-relative">
-                    <input
-                      onChange={handleInputChange}
-                      value={inputValue}
-                      type="text"
-                      className={`form-control with-button ${inputError ? 'is-invalid' : null}`}
-                      placeholder="examplemail@mail.com"
-                    />
-                    <button
-                      disabled={formLoading}
-                      onClick={handleSubmitClick}
-                      className="btn btn-primary btn-absolute"
-                    >
-                      Subscribe
-                    </button>
-                  </div>
+              {formSent ? (
+                <div className="alert alert-success mt-5 mt-lg-5" role="alert">
+                  <strong>Holy guacamole!</strong> Thanks for your interest!.
                 </div>
-              </>
-            )}
+              ) : (
+                <>
+                  <p className="text-lead text-bold mt-5 mt-lg-5">Get notified when we launch</p>
 
-            <div className="d-flex mt-6 mt-lg-15">
-              <a href="#" className="icon-social is-twitter"></a>
-              <a href="#" className="icon-social is-telegram ms-4"></a>
-            </div>
+                  <div className="mt-4">
+                    <div className="position-relative">
+                      <input
+                        onChange={handleInputChange}
+                        value={inputValue}
+                        type="text"
+                        className={`form-control with-button ${inputError ? 'is-invalid' : null}`}
+                        placeholder="examplemail@mail.com"
+                      />
+                      <button
+                        disabled={formLoading}
+                        onClick={handleSubmitClick}
+                        className="btn btn-primary btn-absolute"
+                      >
+                        Subscribe
+                      </button>
+                    </div>
+                  </div>
+                </>
+              )}
 
-            <div className="text-small mt-6 mt-lg-15">
-              <Link href="terms">
-                <a className="link">Terms of use</a>
-              </Link>
-              <span className="mx-2">|</span>
-              <Link href="privacy">
-                <a className="link">Privacy Policy</a>
-              </Link>
+              <div className="d-flex mt-6 mt-lg-15">
+                <a href="#" className="icon-social is-twitter"></a>
+                <a href="#" className="icon-social is-telegram ms-4"></a>
+              </div>
+
+              <div className="text-small mt-6 mt-lg-15">
+                <Link href="terms">
+                  <a className="link">Terms of use</a>
+                </Link>
+                <span className="mx-2">|</span>
+                <Link href="privacy">
+                  <a className="link">Privacy Policy</a>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
