d41bcca16f51cd81f036e8061da79eab8344d45d
Martin Dobrev <martindobrev87@gmail.com>
2022-06-22 09:04:35 +0300

    Font family removed

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Poppins:wght@300;500;700&display=swap"
+          href="https://fonts.googleapis.com/css2?&family=Poppins:wght@300;500;700&display=swap"
           rel="stylesheet"
         ></link>
       </Head>
